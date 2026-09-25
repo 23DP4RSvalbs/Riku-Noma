ebadba9b9c6a1f3457142594b96f2cacf69b7dfb
Ksenija Lakotko <166509373+23DP3KLako@users.noreply.github.com>
2026-09-25 09:58:16 +0000

    [DEV-3] Rīku un kategoriju CRUD API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,21 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ToolController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/tools', [ToolController::class, 'index']);
+Route::get('/tools/{rik}', [ToolController::class, 'show']);
+Route::get('/categories', [ToolController::class, 'categories']);
+
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    Route::post('/tools', [ToolController::class, 'store']);
+    Route::patch('/tools/{rik}', [ToolController::class, 'update']);
+    Route::delete('/tools/{rik}', [ToolController::class, 'destroy']);
+});
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
